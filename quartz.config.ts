23324e007cebc05eba60ff198a2011b32abad94c
Alessio Marini <marini.alessio1105@gmail.com>
2024-02-23 19:34:34 +0100

    Quartz sync: Feb 23, 2024, 7:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "💻 Ale's Vault",
+    pageTitle: "💻 Alem Vault",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
