8aa447d314855827dccd1c7c5da4a1b12639840c
Alessio Marini <marini.alessio1105@gmail.com>
2024-09-27 00:02:46 +0200

    Quartz sync: Sep 27, 2024, 12:02 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -31,6 +32,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
+    //Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
